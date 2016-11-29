7f61a9e46939ea04b592df57d2dcbf570e1b0c29
Sarup Banskota <sbanskota08@gmail.com>
2016-11-29 20:05:15 +0200

    Setup knock to work with auth0

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_many :requests
+  def self.from_token_payload payload
+    if payload['sub']
+      self.find_or_create_by auth_id: payload['sub']
+    end
+  end
 end
